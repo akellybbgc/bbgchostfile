b48d37b5125fff015dbe77bb3934e72cd2af957c
akellybbgc <75756465+akellybbgc@users.noreply.github.com>
2022-01-12 16:57:47 -0500

    Pull File Changed

--- bashpull.bat
+++ bashpull.bat
@@ -1,2 +1,11 @@
-rmdir /S c:\bbgchostfile 
+#######################################
+# Hosts Git Clone
+# Hosts Copy to etc
+#
+# Git
 git clone https://github.com/akellybbgc/bbgchostfile.git c:\
+del c:\windows\system32\drivers\etc\hosts
+cp c:\bbgchostfile\hosts c:\windows\system32\drivers\etc\hosts
+cp bashpull.bat c:\
+del c:\bbgchostfile\*
+rmdir c:\bbgchostfile
