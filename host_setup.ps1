5349a996bf88b7c3f1f2d2a3cdb8374ccdac8134
akellybbgc <75756465+akellybbgc@users.noreply.github.com>
2022-01-18 11:58:24 -0500

    Update host_setup.ps1

--- host_setup.ps1
+++ host_setup.ps1
@@ -4,7 +4,7 @@
 # Date: 01/18/2022
 #
 #--------------------------
-
+Invoke-WebRequest https://raw.githubusercontent.com/akellybbgc/bbgchostfile/main/updatehosts.ps1 -O c:\updatehosts.ps1
 $action = New-ScheduledTaskAction -Execute "powershell.exe c:\updatehosts.ps1"
 $trigger = New-ScheduledTaskTrigger -Hourly -At 6am
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Host File Update" -Description "Host update"
